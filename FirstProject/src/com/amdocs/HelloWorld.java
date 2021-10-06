d66081028455dfc8f429d3c0f7829d8225465957
Siva OS <syskantechnosoft@users.noreply.github.com>
2021-10-06 06:40:34 -0400

    Add files via upload

---
+++ FirstProject/src/com/amdocs/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.amdocs;
+
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!!");
+		
+		Date today = new Date();
+		System.out.println(today);
+	}
+
+}
